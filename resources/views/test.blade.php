622418f5dda1da61a6b4937c2dae7c8cbc244ec6
kakyoindonut321 <forbiddenikki@gmail.com>
2022-10-11 22:35:24 +0700

    konfigurasi boostrap berantakan

---
+++ resources/views/test.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>test</title>
+    <link rel="stylesheet" href={{ URL::to('/') }}>
+</head>
+<body>
+    <div class="container">
+        <h1 class=" text-center">sdpjpadjpsado</h1>
+        <p class="text-primary">adsasasd</p>
+    </div>
+    
+</body>
+</html>
